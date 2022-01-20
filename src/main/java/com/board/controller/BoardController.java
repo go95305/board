45855bf4b2ce56e19d1e647951549b9bbade6111
KYC <go95305@naver.com>
2022-01-20 21:30:21 +0900

    Initial commit

---
+++ src/main/java/com/board/controller/BoardController.java
@@ -0,0 +1,36 @@
+package com.board.controller;
+
+
+import com.board.dto.BoardDto;
+import com.board.service.BoardService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.annotation.Resource;
+
+@Controller
+@AllArgsConstructor
+public class BoardController {
+
+    @Resource(name="boardService")
+    private BoardService boardService;
+
+    @GetMapping("/")
+    public String list(){
+        return "board/list.html";
+    }
+
+    @GetMapping("/post")
+    public String write(){
+        return "board/write.html";
+    }
+
+    @PostMapping("/post")
+    public String write(BoardDto boardDto) {
+        boardService.savePost(boardDto);
+
+        return "redirect:/";
+    }
+}
